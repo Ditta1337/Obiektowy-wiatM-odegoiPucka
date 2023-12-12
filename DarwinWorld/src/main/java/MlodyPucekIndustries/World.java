53bc57b58a0e521771738443ccdcbeb4fbd7aafc
arturdwornik <arturdittadwornik@gmail.com>
2023-12-12 18:19:57 +0100

    Project structure added

---
+++ DarwinWorld/src/main/java/MlodyPucekIndustries/World.java
@@ -0,0 +1,9 @@
+package MlodyPucekIndustries;
+
+public class World {
+    public static void main(String[] args) {
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+}
